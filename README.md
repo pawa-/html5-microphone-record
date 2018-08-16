2bf53ac8e4753a2bafd600f5d8b802b04fd777b2
pawa <pawa@pawafuru.com>
2018-08-16 15:06:15 +0900

    動作確認用に無駄を可能な限り省く

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# mic-preview-for-ios11
-Websites can access camera and microphone streams from a user's device (user permission is required.)
+# ブラウザで音声入力
 
-iOS11のSafariがカメラとマイクにアクセスできるようになったので、マイクにアクセスしてみました。
-
-### マイクにアクセスしてFFT
-https://kimizuka.github.io/mic-preview-for-ios11/analyse/
+動作確認用
---
+++ index.js
@@ -0,0 +1,71 @@
+(function() {
+
+  'use strict';
+
+  if (!checkIfRecordable()) {
+    return;
+  }
+
+  const start_button = document.getElementById('start-button');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const bar_color = 'green';
+  const num_bar = '128';
+
+  // マイクと接続
+  navigator.mediaDevices.getUserMedia({ audio: true })
+   .then(_handleSuccess)
+   .catch(_handleError)
+
+  function checkIfRecordable() {
+    if (!navigator.mediaDevices) {
+      alert('申し訳ありませんが、ご利用のブラウザでは録音できません');
+      return false;
+    }
+
+    return true;
+  }
+
+  function _handleSuccess(evt) {
+    start_button.addEventListener('click', () => {
+      _handleClick(evt);
+    }, false);
+  }
+
+  function _handleError(err) {
+    console.log(err);
+    alert('なにかエラーが発生しました');
+  }
+
+  function _handleClick(evt) {
+    start_button.classList.add('off');
+
+    // window.webkitAudioContext は iOS上のSafariで動かすのに必要
+    const audio_ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+    const src = audio_ctx.createMediaStreamSource(evt);
+    const analyser = audio_ctx.createAnalyser(evt);
+    analyser.fftSize = 1024;
+    src.connect(analyser);
+
+    let data = new Uint8Array(num_bar);
+    let w = 0;
+
+    setInterval(() => {
+      canvas.width  = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      ctx.fillStyle = bar_color;
+      w = canvas.width / num_bar;
+
+      analyser.getByteFrequencyData(data);
+
+      for (let i = 0; i < num_bar; ++i) {
+        ctx.rect(i * w, canvas.height - data[i] * 2, w, data[i] * 2);
+      }
+
+      ctx.fill();
+    }, 20);
+  }
+
+})();
